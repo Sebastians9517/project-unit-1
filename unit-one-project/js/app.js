c5ee02f147f6e375750867002724b3c9c907f94a
sebastians9517 <sebastians9517@gmail.com>
2020-11-17 12:10:54 -0600

    basic javascript skeleton

---
+++ unit-one-project/js/app.js
@@ -0,0 +1,23 @@
+// Defining constants //
+const textElement = document.getElementById('story-text');
+const choiceButtons = document.getElementById('choices-menu');
+
+
+// Defining variables //
+let gameState = {};
+
+
+// Defining functions //
+function gameStart() {
+
+}
+
+
+function showText(textIndex) {
+
+}
+
+
+function selectChoice(choice) {
+
+}
