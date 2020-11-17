0a4526cdfe0761e9a59290ef8fd97c9477a7c570
sebastians9517 <sebastians9517@gmail.com>
2020-11-17 14:08:57 -0600

    first text and option

--- unit-one-project/js/app.js
+++ unit-one-project/js/app.js
@@ -1,6 +1,21 @@
 // Defining constants //
 const textElement = document.getElementById('story-text');
 const choiceButtons = document.getElementById('choices-menu');
+const textNodes = [
+    {
+        id: 1,
+        text: "You are scavenging through the freshly arrived collection of materials at your local bookstore. The place is closing soon, so you quickly pick up your favourites, plus a few new ones.",
+        choices: [
+            {
+                text: "Continue looking around.",
+                nextText: 2
+            }
+        ]
+    },
+    {
+        id: 2,
+    }
+]
 
 
 // Defining variables //
@@ -9,7 +24,8 @@ let gameState = {};
 
 // Defining functions //
 function gameStart() {
-
+    gameState = {};
+    showTextNode(1)
 }
 
 
